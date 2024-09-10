5687f4c8b43d0de9b6813386452f5272014f5504
Dhananjay Sable <dhananjaysable370@gmail.com>
2024-09-10 21:21:15 +0530

    reactapp_02

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [color, setColor] = useState('');
+
+  const changeColor = (newColor) => {
+    console.log(newColor);
+    setColor(newColor);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={`w-full h-screen flex gap-4 items-center justify-center 
+        ${color === 'zinc'
+          ? 'bg-zinc-600'
+          : color === 'blue'
+          ? 'bg-blue-600'
+          : color === 'purple'
+          ? 'bg-purple-600'
+          : ''
+        }`}
+    >
+      <button
+        onClick={() => changeColor('zinc')}
+        className="px-4 py-2 bg-zinc-700 rounded-md text-white"
+      >
+        Zinc
+      </button>
+      <button
+        onClick={() => changeColor('purple')}
+        className="px-4 py-2 bg-purple-700 rounded-md text-white"
+      >
+        Purple
+      </button>
+      <button
+        onClick={() => changeColor('blue')}
+        className="px-4 py-2 bg-blue-700 rounded-md text-white"
+      >
+        Blue
+      </button>
     </div>
   );
 }
